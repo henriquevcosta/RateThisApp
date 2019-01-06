ec110ca99639c811ecc7ef4ce72f7435819b68b1
Henrique Costa <henrique.costa@feedzai.com>
2019-01-06 19:57:56 +1100

    List detail screen
    Has a list detail screen but not a TableViewController yet

--- RateThi/ListDetailViewController.swift
+++ RateThi/ListDetailViewController.swift
@@ -1,5 +1,5 @@
 //
-//  AddListViewController.swift
+//  ListDetailViewController.swift
 //  RateThi
 //
 //  Created by Henrique Costa on 06/01/2019.
@@ -8,32 +8,28 @@
 
 import UIKit
 
-class AddListViewController: UIViewController {
+// TODO should be UITableViewController
+class ListDetailViewController: UIViewController {
 
-    var previousVC = RootListsTableViewController()
+    var previousVC : RootListsTableViewController = RootListsTableViewController()
 
-    @IBOutlet weak var newListName: UITextField!
+    var selectedItem : RatedList? = nil
 
-    @IBOutlet weak var isImportantSwitch: UISwitch!
+    @IBOutlet weak var listNameLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        if let list = selectedItem {
+            listNameLabel.text = list.name
+        }
         // Do any additional setup after loading the view.
     }
-
-    @IBAction func addTapped(_ sender: Any) {
-        let newList = RatedList()
-        if let name = newListName.text {
-            newList.name = name
-            newList.important = isImportantSwitch.isOn
-
-            previousVC.addNewList(newList)
+    
+    @IBAction func deleteList(_ sender: Any) {
+        if let list = selectedItem {
+            previousVC.deleteList(list)
             navigationController?.popViewController(animated: true)
-
-        } else {
-            print("Bugger, null name from text box")
-            return
         }
     }
 
